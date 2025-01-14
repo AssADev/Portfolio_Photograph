913daba2289cd2c98ae141d99d20acd960106e30
Téo <teo@60fps.fr>
2025-01-14 22:56:12 +0100

    chore: SEO Setup

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -9,4 +9,15 @@ const slug = Array.isArray(route.params.slug) ? route.params.slug.join('/') : ro
 
 // Load Storyblok page :
 const story = await useAsyncStoryblok(slug, { version: 'draft' })
+
+// Update SEO :
+const seo = story.value.content.seo[0]
+
+useSeoMeta({
+	title: seo.title,
+	ogTitle: seo.title,
+	description: seo.description,
+	ogDescription: seo.description,
+	ogImage: seo.og_image.filename || ''
+})
 </script>
